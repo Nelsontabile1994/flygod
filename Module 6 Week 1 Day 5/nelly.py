b3e92ebe662562d04632b5c0a19fc9c4be7ee0b5
nelsontabile1994 <57621295+nelsontabile1994@users.noreply.github.com>
2020-02-19 09:22:19 +0200

    Add files via upload

---
+++ Module 6 Week 1 Day 5/nelly.py
@@ -0,0 +1,4 @@
+import pandas as pd
+data = pd.read_csv("Sprint_1.csv", sep=";")
+data
+
---
+++ Module 6 Week 1 Day 5/sprint 3.sql
@@ -0,0 +1,4 @@
+SET SQL_SAFE_UPDATES = 0;
+update my_first.sprint_1
+set barcode=105
+where product ="Bananas(Prepacks)"
